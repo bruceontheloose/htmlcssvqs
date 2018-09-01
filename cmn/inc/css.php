e0ff34e567fb4749a3c858ca8cf77ad0cd2ae6dc
bruceontheloose <bruce@bruceontheloose.com>
2018-09-01 11:41:04 -0700

    Add latest GA code.

--- cmn/inc/css.php
+++ cmn/inc/css.php
@@ -1,11 +1,14 @@
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<!-- <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet" /> -->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-125025519-3"></script>
+  <script>function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","UA-125025519-3");</script>
 	<link rel="stylesheet" href="/cmn/css/normalize.css" />
 	<link rel="stylesheet" href="/cmn/css/base-v2.css" />	
-
 	<!--[if lt IE 9]>
 	<script src="/cmn/js/html5shiv.js"></script>
 	<script src="/cmn/js/respond.min.js"></script>
 	<![endif]-->
-
+  <?php /*
+  <!-- <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet" /> -->
 	<!-- <link rel="shortcut icon" href="/favicon.ico" /> -->
+  */
+  ?>
